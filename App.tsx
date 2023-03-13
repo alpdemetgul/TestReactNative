2ec0776fb332d572cdca51702a084e9d5a7af6c0
Adylsha Yumayev <adylshay@gmail.com>
2023-03-13 17:54:27 +0300

    login request done

--- App.tsx
+++ App.tsx
@@ -1,32 +1,79 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView,TextInput,Button,Alert } from 'react-native';
+import { useState } from 'react';
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
 import { SvgUri } from 'react-native-svg';
 
-
 export default function App() {
+  const [loginFields, setLoginFields] = useState<Object>({
+    username: '',
+    password: '',
+  });
+
+  const handleSubmit = async () => {
+
+    const config = {
+      headers: {
+        'App-Company': 'GLC',
+        'App-Branch': 'MRK',
+        'Accept-Language': 'tr-TR',
+        'Content-Type': 'application/json',
+        externalRequest: 1,
+      },
+    };
+
+    axios
+      .post(
+        'https://colarxapi.gelecekvarlik.com.tr/api/auth/login',
+        loginFields,
+        config
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => console.log('zorttt', error));
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-   <SvgUri
-    width={200}
-    height={200}
-    uri="https://www.gelecekvarlik.com.tr/slider/9443bd65dc8fd7623a5e54350fa31e41.svg"
-/>
-<Text style={styles.title}>Gelecek Varlık</Text>
+      <SvgUri
+        width={200}
+        height={200}
+        uri='https://www.gelecekvarlik.com.tr/slider/9443bd65dc8fd7623a5e54350fa31e41.svg'
+      />
+      <Text style={styles.title}>Gelecek Varlık</Text>
 
       <Text style={styles.text}>Kullanıcı Adı:</Text>
-      <TextInput style={styles.input}></TextInput>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) =>
+          setLoginFields({ ...loginFields, username: text })
+        }
+      />
       <Text style={styles.text}>Şifre:</Text>
-      <TextInput style={styles.input}></TextInput>
-      <StatusBar style="auto" />
-      <Button title='Enter' color="#f5a623"
-      onPress={() => Alert.alert('Simple Button pressed')}/>
-            <Button title='Sign Up!' color="#f5a623"
-      onPress={() => Alert.alert('Simple Button pressed')}/>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) =>
+          setLoginFields({ ...loginFields, password: text })
+        }
+      />
+      <StatusBar style='auto' />
+      <Button title='Enter' color='#f5a623' onPress={handleSubmit} />
+      <Button
+        title='Sign Up!'
+        color='#f5a623'
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
     </SafeAreaView>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -38,27 +85,24 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
-    width:300,
+    width: 300,
     borderWidth: 1,
     padding: 10,
-    backgroundColor:'#fff',
-    borderRadius:50
+    backgroundColor: '#fff',
+    borderRadius: 50,
   },
-  button:{
-    
+  button: {},
+  title: {
+    fontSize: 30,
+    color: '#fff',
+    marginBottom: 40,
   },
-  title:{
-    fontSize:30,
-    color:'#fff',
-    marginBottom:40
-
-  },
-  text:{
-    color:'#fff'
+  text: {
+    color: '#fff',
   },
   logo: {
     width: 100,
-    height:100,
-    marginBottom:40
+    height: 100,
+    marginBottom: 40,
   },
 });
